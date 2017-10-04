fe3e8481191a3c612444f426f8c222c536f432c1
atompkins <andy.tompkins@sony.com>
2017-10-04 09:59:20 -0700

    Switch to using the prop-types package for PropTypes

--- src/ItemRange.js
+++ src/ItemRange.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { Text, View } from 'react-native';
 import JSONArrow from './JSONArrow';
 
--- src/JSONArrow.js
+++ src/JSONArrow.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 const JSONArrow = ({ arrowStyle, expanded, nodeType, onPress, styling }) => (
